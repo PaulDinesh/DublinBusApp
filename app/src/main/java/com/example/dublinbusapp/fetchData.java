ddf13da7c6992e74dc7b867ed1a55be11f9f3158
PaulDinesh <pauldinesh2008@gmail.com>
2019-07-18 23:46:13 +0100

    updated master equivalent to additional feature

--- app/src/main/java/com/example/dublinbusapp/fetchData.java
+++ app/src/main/java/com/example/dublinbusapp/fetchData.java
@@ -21,35 +21,99 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
     String data ="";
     String dataParse = "";
     String singleParse="";
+    String stopid = null;
     @Override
     protected Void doInBackground(Void... voids) {
         try {
             //URL url= new URL("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=7602&format=json");
             //URL url= new URL("http://api.myjson.com/bins/j5f6b");
-            URL url= new URL("https://api.myjson.com/bins/96k67");
+            URL url = new URL("https://api.myjson.com/bins/96k67");
             HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
             InputStream inputStream = httpsURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 //            String jsonStr = sh.makeServiceCall(url);
 
 
-        String line = "";
-            while(line!=null)
-            {
+            String line = "";
+            while (line != null) {
                 line = bufferedReader.readLine();
-            data =data+line;
+                data = data + line;
             }
 //            data = String.format("%s%s", data, line);}
 
-
             JSONObject JA = new JSONObject(data);
             JSONArray jr = JA.getJSONArray("results");
-            for(int i=0;i<jr.length();i++)
-            {
+            System.out.println(jr);
+            stopid = (String) JA.getString("stopid");
+            String timestamp = JA.getString("timestamp");
+            for (int i = 0; i < jr.length(); i++) {
                 JSONObject jb1 = jr.getJSONObject(i);
-                String arrivaldatetime = jb1.getString("arrivaldatetime");
-                Log.i(".......",arrivaldatetime);
-            }
+
+                try {
+                    //stopid = jb1.getString("stopid");
+
+                    //  String timestamp = jb1.getString("timestamp");
+                    String arrivaldatetime = jb1.getString("arrivaldatetime");
+                    String duetime = jb1.getString("duetime");
+                    String departuredatetime = jb1.getString("departuredatetime");
+                    String departureduetime = jb1.getString("departureduetime");
+                    String scheduledarrivaldatetime = jb1.getString("scheduledarrivaldatetime");
+                    String scheduleddeparturedatetime = jb1.getString("scheduleddeparturedatetime");
+                    String destination = jb1.getString("destination");
+                    String destinationlocalized = jb1.getString("destinationlocalized");
+                    String origin = jb1.getString("origin");
+                    String originlocalized = jb1.getString("originlocalized");
+                    String direction = jb1.getString("direction");
+                    String operator = jb1.getString("operator");
+                    String additionalinformation = jb1.getString("additionalinformation");
+                    String lowfloorstatus = jb1.getString("lowfloorstatus");
+                    String route = jb1.getString("route");
+                    String sourcetimestamp = jb1.getString("sourcetimestamp");
+                    String monitored = jb1.getString("monitored");
+                    Log.i(".......\nstopid  ", stopid +
+                            "\narrivaldatetime  " + arrivaldatetime +
+                            "\nduetime  " + duetime +
+                            "\ndeparturedatetime  " + departuredatetime +
+                            "\ndepartureduetime  " + departureduetime +
+                            "\nscheduledarrivaldatetime  " + scheduledarrivaldatetime +
+                            "\nscheduleddeparturedatetime  " + scheduleddeparturedatetime +
+                            "\ndestination  " + destination +
+                            "\ndestinationlocalized  " + destinationlocalized +
+                            "\norigin  " + origin +
+                            "\noriginlocalized  " + originlocalized +
+                            "\ndirection  " + direction +
+                            "\noperator  " + operator +
+                            "\nadditionalinformation  " + additionalinformation +
+                            "\nlowfloorstatus  " + lowfloorstatus +
+                            "\nroute  " + route +
+                            "\nsourcetimestamp  " + sourcetimestamp +
+                            "\noperator  " + operator +
+                            "\ndestinationlocalized  " + destinationlocalized +
+                            "\nmonitored  " + monitored);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                     /*
+                 timestamp"timestamp"
+                 arrivaldatetime"arrivaldatetime"
+                 duetime"duetime"
+                 departuredatetime"departuredatetime"
+                 departureduetime"departureduetime"
+                 scheduledarrivaldatetime"scheduledarrivaldatetime"
+                 scheduleddeparturedatetime"scheduleddeparturedatetime"
+                 destination"destination"
+                 destinationlocalized"destinationlocalized"
+                 origin"origin"
+                 originlocalized"originlocalized"
+                 direction"direction"
+                 operator"operator"
+                 additionalinformation"additionalinformation"
+                 lowfloorstatus"lowfloorstatus"
+                 route"route"
+                 sourcetimestamp"sourcetimestamp"
+                 monitored"monitored"
+            */
 //            for(int i=0;i<JA.length();i++)
 //            {
 //                JSONObject JO = (JSONObject) JA.get(i);
@@ -70,8 +134,8 @@ public class fetchData extends AsyncTask<Void,Void,Void> {
 //                dataParse = String.format("%s%s", dataParse, singleParse);
 //
 //            }
+            }
         }
-
 
         catch (MalformedURLException e) {
             e.printStackTrace();
