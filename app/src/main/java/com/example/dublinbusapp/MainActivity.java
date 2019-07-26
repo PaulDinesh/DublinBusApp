fd68341398aecb5581e7011af3af409637d200bc
PaulDinesh <pauldinesh2008@gmail.com>
2019-07-26 12:28:12 +0100

    Added firebase functionality but work in progress

--- app/src/main/java/com/example/dublinbusapp/MainActivity.java
+++ app/src/main/java/com/example/dublinbusapp/MainActivity.java
@@ -10,29 +10,46 @@ import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    Button click,clicksearch;
+    private static final String TAG = "MainActivity";
+    Button click,clicksearch,btnfirebase;
     public static TextInputEditText search;
     public static TextView data;
+//    DatabaseReference myRef;
+    DatabaseReference database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance().getReference();
+//        myRef = database.getReference("9101");
+        readfromDatabase();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         click = findViewById(R.id.button);
         clicksearch = findViewById(R.id.search_btn);
+        btnfirebase=findViewById(R.id.firebase);
         data = (TextView) findViewById(R.id.fetcheddata);
-search=findViewById(R.id.as);
+        search=findViewById(R.id.as);
         clicksearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,8 +57,6 @@ search=findViewById(R.id.as);
                 process.execute();
             }
         });
-
-
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,25 +67,65 @@ search=findViewById(R.id.as);
                 startActivityForResult(vc,100);
             }
         });
+        btnfirebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ViewDatabase.class);
+            }});
+    }
+    public void readfromDatabase(){// Read from the database
+        database.addValueEventListener(new ValueEventListener() {
+//            database.orderByChild("PlateCode").equalTo("339072").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+//                String childvalue = String.valueOf(dataSnapshot.child("PlateCode").getValue());
+                showData(dataSnapshot);
+//                Log.d("paul", "Value is: " + childvalue);
+//                String childvalue1 = String.valueOf(dataSnapshot.getValue());
+//
+//                Log.d("Data", "Value is: " + childvalue1);
+
+            }
+
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                //                String value = dataSnapshot.getValue(String.class);
+//
+//                String childvalue = String.valueOf(dataSnapshot.getValue());
+//                showData(dataSnapshot);
+//                Log.d("paul", "Value is: " + childvalue);
+//            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Error", "Failed to read value.", error.toException());
+            }
+        });
+}
+    private void showData(DataSnapshot dataSnapshot) {
+        for (DataSnapshot ds : dataSnapshot.getChildren()){
+            Log.d("loop", "Value is: " + ds);
+        }
 
     }
 
     @Override
     protected void onActivityResult( int requestcode,int resultCode,@Nullable Intent data){
-super.onActivityResult(requestcode,resultCode,data);
+        super.onActivityResult(requestcode,resultCode,data);
 
-switch (requestcode){
-    case 100:{
-        if(resultCode==RESULT_OK&&null!=data){
-            ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            search.setText(result.get(0));
-            fetchData process = new fetchData();
-                process.execute();
+        switch (requestcode){
+            case 100:{
+                if(resultCode==RESULT_OK&&null!=data){
+                    ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    search.setText(result.get(0));
+                    fetchData process = new fetchData();
+                    process.execute();
+                }
+                break;
+            }
         }
-        break;
-    }
-}
 
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/dublinbusapp/StopInformation.java
@@ -0,0 +1,44 @@
+package com.example.dublinbusapp;
+
+public class StopInformation {
+
+    private String PlateCode;
+    private String CommonName_lang_en;
+    private String ShortCommonName_lang_en;
+    private String Street_lang_en;
+
+    public String getPlateCode() {
+        return PlateCode;
+    }
+
+    public void setPlateCode(String plateCode) {
+        PlateCode = plateCode;
+    }
+
+    public String getCommonName_lang_en() {
+        return CommonName_lang_en;
+    }
+
+    public void setCommonName_lang_en(String commonName_lang_en) {
+        CommonName_lang_en = commonName_lang_en;
+    }
+
+    public String getShortCommonName_lang_en() {
+        return ShortCommonName_lang_en;
+    }
+
+    public void setShortCommonName_lang_en(String shortCommonName_lang_en) {
+        ShortCommonName_lang_en = shortCommonName_lang_en;
+    }
+
+    public String getStreet_lang_en() {
+        return Street_lang_en;
+    }
+
+    public void setStreet_lang_en(String street_lang_en) {
+        Street_lang_en = street_lang_en;
+    }
+
+
+
+}
